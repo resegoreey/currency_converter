7b2deebd6c96cfecbaf94574af8c1c2fc8fb1bfe
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-29 21:46:40 +0200

    Wrote basic code for currency conversion betweer dollar and rand

---
+++ currency.py
@@ -0,0 +1,8 @@
+print("Convert from Dollars($) to Rands(R)")
+
+#take dollars input from the user
+dollar = int(input("Enter your $ to convert: "))
+
+#conversion to rand
+rand = dollar * 18.56
+print(f"R{rand}")
